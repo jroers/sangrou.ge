e72649e3580b01b18445d37d2ecb775e5c75fc56
jroers <joe.roers@gmail.com>
2016-02-19 10:58:41 -0800

    Techs can be created on the org pseudo-'show' page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,34 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def create_tech
+		p "****** PARAMETERS ARE HERE ****** #{params}"
+		if domain_matches?
+			@user = User.new(tech_params.merge(email: params[:user][:email].downcase ,is_tech?: true))
+			if @user.save
+				redirect_to root_path
+			else
+				redirect_to tech_signup_path(params[:user][:organization][:id])
+				flash[:error] = "Please ensure all fields are entered correctly"
+			end
+		end
+	end
+
+	private
+
+	def tech_params
+		params.require(:user).permit(:first, :last, :password)
+	end
+
+	def domain_matches?
+		user_email = params[:user][:email].downcase
+		expected_domain = params[:user][:organization_domain]
+		if user_email[-1 * expected_domain.length, expected_domain.length]
+			return true
+		else
+			redirect_to tech_signup_path(params[:user][:organization_id])
+			flash[:error] = "Please enter a valid email address for this organization."
+			return false
+		end
+	end
 end
