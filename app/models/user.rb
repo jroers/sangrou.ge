f952cbe7946c3ce8f450361bb26b5a0102a9263d
jroers <joe.roers@gmail.com>
2016-02-19 12:20:18 -0800

    Self.confirm method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
 	has_many :organizations, through: :donations
 
 	has_secure_password
+
+	def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user.try(:authenticate, params[:password])
+  end
 end
