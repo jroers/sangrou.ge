25b047860383fa8109013fad3d9f09d8f0323551
jroers <joe.roers@gmail.com>
2016-02-19 12:34:36 -0800

    create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,20 @@ class SessionsController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	def create
+    user_params = params.require(:user).permit(:email, :password)
+    @user = User.confirm(user_params)
+    if @user
+      login(@user)
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+  	logout
+  	redirect_to root_path
+  end
 end
