1f791da7f57bc7b1bd8ad23d6f452a02e1e23990
jroers <joe.roers@gmail.com>
2016-02-22 14:17:36 -0800

    Seeding one org, donor and tech

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+org = Organization.create(
+	name:"GA Blood Center SF", 
+	email_domain: "@gablood.co", 
+	logo:"http://washingtontechnology.org/wp-content/uploads/2014/11/General_Assembly_logo.png", 
+	address1:"225 Bush St.", 
+	address2:"5th Floor", 
+	city:"San Francisco", 
+	state:"CA", 
+	zip:94104
+)
+
+tech = User.create(
+	first: "IP",
+	last: "Phleb",
+	email: "ip.phleb1@gablood.co",
+	is_tech?: true,
+	password: "1234"
+)
+
+donor = User.create(
+	first: "Example",
+	last: "Donor",
+	phone: "4155555555",
+	dob: "1990-01-01",
+	address1: "225 Bush St",
+	address2: "5th Floor",
+	city: "San Francisco",
+	state: "CA",
+	zip: 94104,
+	email: "first.donor@ga.co",
+	password: "1234"
+)
