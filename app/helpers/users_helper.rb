631b10305345d7ad9e1ea211d02aa3bbb8b36507
jroers <joe.roers@gmail.com>
2016-02-22 16:49:29 -0800

    Added users helper with some methods to determine whether a user is a tech and what organization they're affiliated with

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+	
+	def tech?
+		current_user
+		if @current_user
+			@current_user.is_tech?
+		end
+	end
+
+	def affiliated_org?
+		if tech?
+			@org = Organization.find_by_id(@current_user.organization_id)
+		end
+	end
+
 end
